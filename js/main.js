84b6f76e15d3f28cc783f6ff4c7381c9cea48c2d
julian <julian.gio.wieckardt@gmail.com>
2019-01-15 23:57:28 -0500

    menu

--- js/main.js
+++ js/main.js
@@ -1,15 +1,8 @@
+$(".accordion").click(function()
+  {
+// $(this).find('.panel').toggleClass('active')
+$(this).parent().find('.panel').toggleClass('active')
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+  }
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
+);
